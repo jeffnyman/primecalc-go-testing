face6e554ea0ac8c92b756ac8ef2c652154f4609
Jeff Nyman <jeffnyman@gmail.com>
2025-01-10 18:18:46 -0600

    Initial prime check implementation.

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	data := 2
+
+	_, outcome := checkPrime(data)
+
+	fmt.Println(outcome)
+}
+
+func checkPrime(data int) (bool, string) {
+	if data == 0 || data == 1 {
+		return false, fmt.Sprintf("%d is not prime", data)
+	}
+
+	if data < 0 {
+		return false, "negative numbers are not prime"
+	}
+
+	for i := 2; i <= data/2; i++ {
+		if data%i == 0 {
+			return false, fmt.Sprintf("%d is not prime; divisible by %d", data, i)
+		}
+	}
+
+	return true, fmt.Sprintf("%d is prime", data)
+}
