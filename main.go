7630a4e392ea883d1f60629b1f591bc423f5a9ea
Jeff Nyman <jeffnyman@gmail.com>
2025-01-12 04:34:51 -0600

    Add user input.

--- main.go
+++ main.go
@@ -1,13 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	data := 2
+	startup()
 
-	_, outcome := checkPrime(data)
+	exitChannel := make(chan bool)
 
-	fmt.Println(outcome)
+	go getInput(exitChannel)
+
+	<-exitChannel
+
+	close(exitChannel)
+
+	fmt.Println("Exiting")
+}
+
+func getInput(exitChannel chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		result, done := getNumber(scanner)
+
+		if done {
+			exitChannel <- true
+			return
+		}
+
+		fmt.Println(result)
+		prompt()
+	}
+}
+
+func getNumber(scanner *bufio.Scanner) (string, bool) {
+	scanner.Scan()
+
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	number, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Enter a whole number", false
+	}
+
+	_, outcome := checkPrime(number)
+
+	return outcome, false
+}
+
+func startup() {
+	fmt.Println("Is number prime?")
+	fmt.Println("Enter a whole number; q to quit.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("> ")
 }
 
 func checkPrime(data int) (bool, string) {
